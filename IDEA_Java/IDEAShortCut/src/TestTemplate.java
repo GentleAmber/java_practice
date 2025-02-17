cf4ab00574b5d8495d800cead2fb96e5f9ad518a
Amber <Amber123@163.com>
2025-02-17 16:36:57 +0000

    IDEA Introduction

---
+++ IDEA_Java/IDEAShortCut/src/TestTemplate.java
@@ -0,0 +1,18 @@
+public class TestTemplate {
+    //Settings - Editor - Live Template
+    //main
+    public static void main(String[] args) {
+        //sout
+        System.out.println("Hello IDEA!");
+        //St = String
+        String name;
+        //fori = creates a loop
+        for (int i = 0; i < 6; i++) {
+            for (int j = 0; j < 3; j++) {
+
+            }
+
+        }
+    }
+
+}
---
+++ IDEA_Java/Intro/src/HelloIDEA.java
@@ -0,0 +1,5 @@
+public class HelloIDEA {
+    public static void main(String[] args){
+        System.out.println("Hello IDEA!!");
+    }
+}
