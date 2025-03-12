e179c249645f45f4828a15c02d7bd82a28642c5e
Amber <Amber123@163.com>
2025-03-12 10:10:56 +0000

    “Letting management system"

--- IDEA_Java/chapter09/src/lettingsystem/MainLoop.java
+++ IDEA_Java/chapter09/src/lettingsystem/MainLoop.java
@@ -1,2 +1,42 @@
-package lettingsystem;public class MainLoop {
+package lettingsystem;
+
+import java.util.Scanner;
+
+public class MainLoop {
+    public static void main(String[] args) {
+        int idAutomated = 0;
+
+        Scanner scanner = new Scanner(System.in);
+        char selection;
+        Property[] properties = new Property[0];
+
+        Menu menu = new Menu();
+        do {
+            menu.showMainMenu();
+
+            selection = scanner.next().charAt(0);
+            switch (selection) {
+                case '1':
+                    idAutomated++;
+                    properties = new LettingSystem().addProperty(properties, idAutomated);
+                    break;
+                case '2':
+                    new LettingSystem().searchProperty(properties);
+                    break;
+                case '3':
+                    properties = new LettingSystem().deleteProperty(properties);
+                    break;
+                case '4':
+                    new LettingSystem().changeInfo(properties);
+                    break;
+                case '5':
+                    new LettingSystem().showPropertyList(properties);
+                    break;
+                case '6':
+                    break;
+                default:
+                    System.out.println("Wrong input.");
+            }
+        }while(selection != '6');
+    }
 }
--- IDEA_Java/chapter09/src/lettingsystem/Test.java
+++ IDEA_Java/chapter09/src/lettingsystem/Test.java
@@ -1,2 +1,16 @@
-package lettingsystem;public class Test {
+package lettingsystem;
+
+import java.util.Scanner;
+
+public class Test {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Type in something without space: ");
+        String a = scanner.nextLine();
+        System.out.println("Type in something else: ");
+        String  b = scanner.nextLine();
+
+        System.out.println("You've just typed: " + a + " " + b);
+
+    }
 }
